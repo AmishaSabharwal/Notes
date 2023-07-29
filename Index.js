247beeaacef6a52b17de185dafa1aaf6011587ba
AmishaSabharwal <124896176+AmishaSabharwal@users.noreply.github.com>
2023-07-29 20:10:11 +0530

    Delete Index.js

--- Index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import reactDOM from 'react-dom';
-import { createRoot } from 'react-dom/client';
-import './Style.css';
-import App from './App';
-
-createRoot(document.getElementById('root')).render(<App/>);
