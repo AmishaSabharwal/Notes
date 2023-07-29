c02b68c012ef3f3c94a3327e79deca64557772ae
AmishaSabharwal <124896176+AmishaSabharwal@users.noreply.github.com>
2023-07-29 20:15:04 +0530

    Add files via upload

---
+++ Notes.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import App from './App';
+
+const react = require('react');
+function Notes(){
+    return(
+        <div className='note'>
+    <h1>Notes</h1>
+    <p>this to remember</p>
+    </div>
+    );
+}
+export default Notes;
---
+++ index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import './Style.css';
+import App from './App';
+
+createRoot(document.getElementById('root')).render(<App/>);
+
+
+// const react = require("react");
+// const reactDOM= require("react-dom");
+// createRoot(document.getElementById('root')).render(
+//   <div>
+//     <Heading />
+//     <Footer/>
+//   </div>
+// );
+
